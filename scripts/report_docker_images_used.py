8685f2a69a24c48ded194d3b35785c0211098cdb
Harry Watkinson <harryw@peachmedia.com>
2024-06-29 15:56:01 +0100

    remove golang image

--- scripts/report_docker_images_used.py
+++ scripts/report_docker_images_used.py
@@ -36,21 +36,21 @@ def display_docker_images(display_eso_langs: bool = True) -> None:
             f"{get_project_root()}/projects/{project}/{dockerfile_name[0]}", "r"
         ) as file:
             # Get image counts
-            if docker_image := IMAGE_PATTERN.findall(file.read())[0]:
-                checked += 1
+            docker_image = IMAGE_PATTERN.findall(file.read())[0]
+            checked += 1
 
-                if "esolang" in docker_image:
-                    eso_langs_count += 1
-                    if not display_eso_langs:
-                        continue
+            if not docker_image:
+                raise Exception(f"Image not found in {project}")
 
-                if docker_image in docker_images:
-                    docker_images[docker_image] += 1
-                else:
-                    docker_images[docker_image] = 1
+            if "esolang" in docker_image:
+                eso_langs_count += 1
+                if not display_eso_langs:
+                    continue
 
+            if docker_image in docker_images:
+                docker_images[docker_image] += 1
             else:
-                raise Exception(f"No match found for {project}")
+                docker_images[docker_image] = 1
 
     if checked != len(projects):
         raise Exception("Not all projects checked")
@@ -68,9 +68,13 @@ def display_results(docker_images_sorted: dict, eso_langs_count: int) -> None:
     print("\nIdeally each project should use the ubuntu:20.04 base image...\n")
     print(f"Number of different images: \t{len(docker_images_sorted.keys()) + eso_langs_count}")
     print(f"Esolangs count: \t\t\t\t{eso_langs_count}\n")
-    print("Count\tDocker Image")
-    for image, count in docker_images_sorted.items():
-        print(f"{count} \t\t{image}")
+    print("Count\t\tDocker Image")
+
+    # Inelegant way to display the results...
+    docker_images_sorted_list = list(docker_images_sorted.items())
+    print(f"{docker_images_sorted_list[0][1]} \t\t{docker_images_sorted_list[0][0]}")
+    for image, count in docker_images_sorted_list[1:]:
+        print(f"{count} \t\t\t{image}")
 
 
 if __name__ == "__main__":
