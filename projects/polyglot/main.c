ed6d8651a4ef35712024097a0045e0a26f9b0e2f
Harry Watkinson <harryw@peachmedia.com>
2025-01-14 17:39:41 +0000

    fix a few df and pg wip

---
+++ projects/polyglot/main.c
@@ -0,0 +1,51 @@
+ #/*/**/if 0//<?php echo "\r";/*
+ #[
+ ###
+ #`{{
+ #=
+q="""" ":" "=;q^"
+true||\
+=begin
+ ###
+ #*/
+ ###]#
+echo "Hello, World!";##[]
+exit
+/* :'
+ ###
+ #*/
+ ###
+("\n\r");?>
+++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<++++++++.--------.+++.------.--------.>+.>..>[
+ #endif
+ #include<stdio.h>
+int main(){printf("Hello, World!\n");return 0;}
+ #if 0
+ ###
+ console.log "Hello, World!"
+ ###
+=#
+println("Hello, World!")
+ #=
+\begin{code}%
+open import Agda.Builtin.IO
+open import Agda.Builtin.String
+data Unit : Set where
+  tt : Unit
+{-# COMPILE GHC Unit = data () (()) #-}
+postulate
+  putStrLn : String -> IO Unit
+{-# FOREIGN GHC import qualified Data.Text as T #-}
+{-# COMPILE GHC putStrLn = putStrLn . T.unpack #-}
+\end{code}%
+\begin{code}
+main = putStrLn "Hello, World!"
+\end{code}
+=end
+" """
+ #}}
+ # ^;
+print("Hello, World!");
+ # =###
+ #*/[doc=r"\"]#[doc="
+ #endif //"]fn main(){println!("Hello, World!")}//[]#]## #'
---
+++ projects/polyglot/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo -n "Bash: " && bash main.c
+gcc main.c -o main && echo -n "C: " && ./main
+g++ main.c -o main && echo -n "C++: " && ./main
+#echo -n "CoffeeScript: " && coffee main.c
+echo -n "Fish: " && fish main.c
+echo -n "Julia: " && julia main.c
+echo -n "Perl: " && perl main.c && echo ""
+#echo -n "PHP: " && php main.c
+echo -n "Python 2: " && python2 main.c
+echo -n "Python 3: " && python3 main.c
+echo -n "Ruby: " && ruby main.c && echo ""
+rustc main.c -o main.rust && echo -n "Rust: " && ./main
+echo -n "Nim: " && ./main
+echo -n "Sh: " && sh main.c
+#echo -n "Tsh: " && tsh main.c
+echo -n "Zsh: " && sh main.c
